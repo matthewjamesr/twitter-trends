c204708663bc5f7be2f95e2f192e63562f2ac268
Matthew Reichardt <me@matthewreichardt.com>
2015-09-21 14:59:27 -0500

    Moved hash code

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -9,15 +9,27 @@ class QueriesController < ApplicationController
       flash[:notice] = "You must enter a search term."
     end
     if params[:query] != ""
-      @hashtags = API.hashtags(params[:query])
+      hashtags = API.hashtags(params[:query])
+      @hashtags = hashtags(hashtags)
     end
   end
 
-  def highlight(text, search_string)
-    keywords = search_string.squeeze.strip.split(" ").compact.uniq
-    matcher = Regexp.new( '(' + keywords.join("|") + ')' )
-    highlighted = text.gsub(matcher) { |match| "#{match}" }
+  #def highlight(text, search_string)
+  #  keywords = search_string.squeeze.strip.split(" ").compact.uniq
+  #  matcher = Regexp.new( '(' + keywords.join("|") + ')' )
+  #  highlighted = text.gsub(matcher) { |match| "#{match}" }
 
-    return highlighted
+  #  return highlighted
+  #end
+
+  def hashtags(data)
+    hash = {}
+    data.each do |d|
+      d.hashtags.each do |d|
+        if d.text != nil
+          hash[:"#{d.text.to_s}"]
+        end
+      end
+    end
   end
 end
