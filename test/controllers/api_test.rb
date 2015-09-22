2ff61111e116a0d8fcbb4bacc24bd50f58aef0ea
Matthew Reichardt <me@matthewreichardt.com>
2015-09-22 01:48:47 -0500

    Updated tests

--- test/controllers/api_test.rb
+++ test/controllers/api_test.rb
@@ -6,12 +6,17 @@ class ApiTest < ActiveSupport::TestCase
     assert(API.makecall("Hello", 1), msg = "API did not respond")
   end
 
+  def test_that_query_is_not_empty
+    query = "Hello"
+    assert_not_nil(API.makecall(query, 1), msg = "API did not recieve a query")
+  end
+
   def test_that_api_returns_500_tweets
     @times = 0
     API.makecall("news", 500).each do |r|
       @times = @times + 1
     end
-    assert_equal 500, @times, "Did not return 100 tweets"
+    assert_equal 500, @times, "Did not return 500 tweets"
   end
 
 end
