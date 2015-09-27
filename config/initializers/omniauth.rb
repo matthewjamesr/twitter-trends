fa9c737e1413f08fb8a31be697872b06b7fe2b34
Matthew Reichardt <me@matthewreichardt.com>
2015-09-26 21:39:32 -0500

    Changed twitter init

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,10 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV["TWITTER_KEY"], ENV["TWITTER_SECRET"],
   {
     :secure_image_url => 'true',
-    :image_size => 'original',
-    :authorize_params => {
-      :force_login => 'true'
-    }
+    :image_size => 'original'
   }
 
 end
