49842df4edebbfb09afd9d13eea8a8d701dac293
Matthew Reichardt <me@matthewreichardt.com>
2015-09-26 22:12:07 -0500

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   has_many :searches
 
   validates_format_of :email, :with => /@/
-  validates :first_name, length: { minimum: 3, message: "A name must be more than three letters" }
-  validates :last_name, length: { minimum: 3, message: "A name must be more than three letters" }
+  #validates :first_name, length: { minimum: 3, message: "A name must be more than three letters" }
+  #validates :last_name, length: { minimum: 3, message: "A name must be more than three letters" }
 
   def self.find_or_create_from_auth_hash(auth_hash)
     User.where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create do |u|
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
       u.twitter_secret = auth_hash.credentials.secret,
       u.provider = auth_hash.provider,
       u.uid = auth_hash.uid
+      u.save
     end
   end
 end
